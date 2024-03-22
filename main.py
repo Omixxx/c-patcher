14937be14cfad6eda7e94f2c9b57be4b5242cb7c
Omixxx <xxcortex99@gmail.com>
2024-03-22 15:05:19 +0100

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from modules.types.TsvFileInput import TsvFileInput
 import modules.utils as utils
+import modules.diff_evaluator as diff
 
 
 def validate_args(path: str, path_to_patches: str):
@@ -20,14 +21,18 @@ def validate_args(path: str, path_to_patches: str):
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         raise ValueError("Invalid number of arguments")
-    path = sys.argv[1]
-    path_to_patches = sys.argv[2]
+    tsv_file_path = sys.argv[1]
+    original_project = sys.argv[2]
+
+    diff.compute_diffs(tsv_file_path, "abstract_method", "model_prediction", "is_diff")
 
     # validate_args(path, path_to_patches)
-    rows: list[TsvFileInput] = utils.extract_rows(path_to_patches)
-    os.chdir(path)
-    for row in rows:
-        row = utils.remove_tokens(row)
 
-    print(rows[10].is_diff)
+    # rows: list[TsvFileInput] = utils.extract_rows(path_to_patches)
+    # os.chdir(path)
+    # for row in rows:
+    #     row = utils.remove_tokens(row)
+    #
+    # print(rows[10].is_diff)
+
     # apply_patches(patch_objects)
