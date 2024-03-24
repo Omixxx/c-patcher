4f029467bc1c3d411e2914b05e9c76c026991a5a
Omixxx <xxcortex99@gmail.com>
2024-03-24 16:31:35 +0100

    first implementation of the process utility
    this utility allows you to process one method at a time showing the original
    method and the partially detokenized one. the program opens an in
    stance of vim and allows you to edit the method, if the method changes
    then it exports it to a .txt file

---
+++ processor.py
@@ -0,0 +1,67 @@
+import os
+import hashlib
+import sys
+import subprocess
+import modules.utils as utils
+from modules.custom_types.TsvFileInput import TsvFileInput
+
+TEMP_FILE = "temp_file.txt"
+ESCAPE_CHAR = "#"
+
+
+def create_file(file_path: str, content: str = ""):
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as file:
+            file.write(content)
+
+
+def calculate_hash(file_path: str) -> str:
+    hash_obj = hashlib.new("sha256")
+    with open(file_path, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            hash_obj.update(chunk)
+    return hash_obj.hexdigest()
+
+
+def prepare_to_manual_evaluation(row: TsvFileInput):
+
+    with open(TEMP_FILE, "w") as file:
+        for line in row.originalMethod.split("\n"):
+            file.write(f"{ESCAPE_CHAR}{line}\n")
+        file.write(f"{row.partially_detokenized_method}")
+
+
+def get_detokenized_method(file_path: str):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    output = ""
+    for line in lines:
+        if not line.startswith(ESCAPE_CHAR):
+            output += line
+    create_file("out.txt", output)
+
+
+def evaluate(rows: list[TsvFileInput]):
+    create_file(TEMP_FILE)
+    i = 0
+    for row in rows:
+        if ++i > 3:
+            continue
+        i += 1
+        if not row.is_diff:
+            continue
+
+        prepare_to_manual_evaluation(row)
+        before_hash = calculate_hash(TEMP_FILE)
+        subprocess.run(["nvim", TEMP_FILE])
+        after_hash = calculate_hash(TEMP_FILE)
+
+        if before_hash == after_hash:
+            continue
+        get_detokenized_method(TEMP_FILE)
+
+
+if __name__ == "__main__":
+    tsv_path = sys.argv[1]
+    rows: list[TsvFileInput] = utils.extract_rows(tsv_path)
+    evaluate(rows)
