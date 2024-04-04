8563f9fccd7d2e9a324e03c8e24437be7c38bd61
Omixxx <xxcortex99@gmail.com>
2024-04-04 12:07:37 +0200

    implemented automatic test capabilities

--- test_suite_evaluator.py
+++ test_suite_evaluator.py
@@ -1,9 +1,13 @@
 import sys, os, subprocess
+from modules.custom_types.TsvFileInput import TsvFileInput
 import modules.utils as utils
+import logging
 
 
 def patch(class_path: str, start_line: int, end_line: int, patch: str):
-    with open(class_path, "r") as file:
+    class_path_with_projectname_removed = "/".join(class_path.split("/")[1:])
+    print(class_path_with_projectname_removed)
+    with open(class_path_with_projectname_removed, "r") as file:
         file_lines = file.readlines()
         # lines index start form 0 in file_lines!!
         below_original_method = file_lines[end_line - 1 :]
@@ -11,22 +15,47 @@ def patch(class_path: str, start_line: int, end_line: int, patch: str):
         patched_file = (
             "".join(above_original_method) + patch + "".join(below_original_method)
         )
-        with open(class_path, "w") as file:
-            file.write(patched_file)
+    with open(class_path_with_projectname_removed, "w") as file:
+        file.write(patched_file)
 
+
+def run_test_suite(row: TsvFileInput):
+    module_to_test = row.classPath.split("/")[1]
+    command = ["mvn", "test", "-pl", module_to_test]
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+
+    status_code = process.wait()
+
+    if status_code == 0:
+        logging.info(f"Test suite passed for {row.classPath}")
+        row.does_test_suite_pass = True.__str__()
+    else:
+        logging.info(f"Test suite failed for {row.classPath}")
+        row.does_test_suite_pass = False.__str__()
+
+
+logging.basicConfig(level=logging.INFO)
 tsv_file_path = sys.argv[1]
 project_path = sys.argv[2]
 
 rows = utils.extract_rows(tsv_file_path)
+current_dir = os.getcwd()
 os.chdir(project_path)
 for row in rows:
 
     if row.does_contain_errors == "":
         break
 
-    if bool(row.does_contain_errors) == True:
+    if row.does_contain_errors == True.__str__():
         continue
 
-    patch(row.classPath, int(row.startLine), row.endLine, row.detokenized_method)
-    
-        
+    logging.info(f"Applying patches...")
+    patch(row.classPath, row.startLine, row.endLine, row.detokenized_method)
+    run_test_suite(row)
+    logging.info("Removing patches...")
+    subprocess.run(["git", "checkout", "."])
+    print("\n")
+os.chdir(current_dir)
+utils.update_tsv(tsv_file_path, rows)
